dda3c9fea771a3afabf3c111b81cefead88acacb
rodrigodovalle <rodrigoalv@al.insper.edu.br>
2026-04-30 00:34:54 -0300

    adiciona função guarda_dados

--- funcoes.py
+++ funcoes.py
@@ -27,3 +27,57 @@ def rolar_dados(n):
         resultados.append(dado)
 
     return resultados
+
+# Antes de iniciar este exercício, leia atentamente o enunciado
+# do projeto no site da disciplina para evitar perda de nota.
+
+# No jogo de dados, é possível guardar alguns dos dados rolados
+# para tentar formar uma combinação específica.
+
+# Precisamos implementar a função guardar_dado no arquivo funcoes.py.
+# A função deve receber:
+# uma lista de dados rolados
+# uma lista de dados já guardados
+# um número inteiro representando o índice do dado a ser guardado
+
+# A função deve retornar uma lista com dois valores:
+# o primeiro: lista de dados rolados atualizada
+# o segundo: lista de dados armazenados no estoque
+
+# Exemplo:
+
+# dados_rolados = [1, 3, 2]
+# dados_no_estoque = [1, 2]
+# dado_para_guardar = 1
+
+# print(guardar_dado(dados_rolados,
+#                    dados_no_estoque,
+#                    dado_para_guardar))
+
+# Saída esperada:
+# [[1, 2], [1, 2, 3]]
+
+# Explicação:
+# O jogador já tem [1, 2] guardados.
+# Ele quer guardar o dado de índice 1 da lista [1, 3, 2],
+# ou seja, o valor 3.
+
+# A função deve:
+# remover esse valor da lista dados_rolados
+# adicionar esse valor na lista dados_no_estoque
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    valor = dados_rolados[dado_para_guardar]
+
+    novos_dados_rolados = []
+    for i in range(len(dados_rolados)):
+        if i != dado_para_guardar:
+            novos_dados_rolados.append(dados_rolados[i])
+
+    novos_dados_no_estoque = []
+    for item in dados_no_estoque:
+        novos_dados_no_estoque.append(item)
+
+    novos_dados_no_estoque.append(valor)
+
+    return [novos_dados_rolados, novos_dados_no_estoque]

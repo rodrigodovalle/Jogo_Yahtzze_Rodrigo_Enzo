31428cd6d55063d75ede52a4ad5d8fda3921629c
rodrigodovalle <rodrigoalv@al.insper.edu.br>
2026-05-01 13:34:01 -0300

    pontos_regra_avancada

--- funcoes.py
+++ funcoes.py
@@ -234,3 +234,16 @@ def calcula_pontos_quina(dados):
 
     return 0
 
+# Ex 11
+
+def calcula_pontos_regra_avancada(dados):
+    resultado = {}
+
+    resultado['cinco_iguais'] = calcula_pontos_quina(dados)
+    resultado['full_house'] = calcula_pontos_full_house(dados)
+    resultado['quadra'] = calcula_pontos_quadra(dados)
+    resultado['sem_combinacao'] = calcula_pontos_soma(dados)
+    resultado['sequencia_alta'] = calcula_pontos_sequencia_alta(dados)
+    resultado['sequencia_baixa'] = calcula_pontos_sequencia_baixa(dados)
+
+    return resultado

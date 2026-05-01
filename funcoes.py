dc83910d65402b8042859d6a08ad742f2f6a3c76
enzowliran <enzowliran@gmail.com>
2026-05-01 13:02:34 -0300

    calcula_pontos_sequencia_alta

--- funcoes.py
+++ funcoes.py
@@ -161,4 +161,15 @@ def calcula_pontos_sequencia_baixa(dados):
 
     return 0
 
+# Ex 7
+
+def calcula_pontos_sequencia_alta(dados):
+
+    if (1 in dados and 2 in dados and 3 in dados and 4 in dados and 5 in dados):
+        return 30
+
+    if (2 in dados and 3 in dados and 4 in dados and 5 in dados and 6 in dados):
+        return 30
+
+    return 0
 
